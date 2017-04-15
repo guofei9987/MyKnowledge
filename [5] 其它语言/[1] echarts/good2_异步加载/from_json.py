3c298b06322b62e57fa866c6573a6ab839a5c25a
guofei9987 <guofei9987@foxmail.com>
2017-04-15 13:17:56 +0800

    AutoUpload

---
+++ [5] 其它语言/[1] echarts/good2_异步加载/from_json.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#b=pd.DataFrame({"categories": ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],"data": [5, 20, 36, 10, 10, 20]})
+
+b=pd.DataFrame({"categories": [["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]],"data": [[5, 20, 36, 10, 10, 100]]},index=['idx'])
+print(b)
+b.to_json('d.json',orient='index')
+
+#c=pd.read_json('data.json')
+#print(c)
+#
+# import os
+# os.system('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files')
